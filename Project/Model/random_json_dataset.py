6552e337e0c505f420b92631e8cf79d63ba766ea
saharH1989 <47110847+saharH1989@users.noreply.github.com>
2019-02-13 20:55:35 +0330

    Sahar Hosseini: this file changed to create data

--- Project/Model/random_json_dataset.py
+++ Project/Model/random_json_dataset.py
@@ -1,6 +1,6 @@
 import json
 import random
-
+import numpy as np
 question_number = 20
 student_number = 100
 
@@ -11,7 +11,7 @@ def main():
     # For each student
     for i in range(student_number):
         # We generate a student id (for example: student_12)
-        student_id = 'student_' + str(i)
+        student_id ='student_' + str(i)
         # This student will hold a structure. This will be all
         # his answers
         data[student_id] = {}
@@ -23,13 +23,37 @@ def main():
 
             # The result variable will hold the answers of the student
             # for that question. This is formaated as a string like 0;0;1;0
-            result = ';'.join(str(random.randint(0, 1)) for k in range(4))
-            data[student_id][question_id] = result
-
+           # result=[str(random.randint(0, 1)) for k in range(4)]
+            result ={}# ';'.join(str(random.randint(0, 1)) for k in range(4))
+            for k in range(4):
+                result[k] = str(random.randint(0, 1))
+            data[student_id][question_id]=result
     # Once the data is generated, we save it in a file 'dataset.json' with
     # indentation so that the result is readable for a human
-    with open('dataset.json', 'w') as out:
+    with open('dataset1.json', 'w') as out:
+        json.dump(data, out, indent=2)
+
+
+def main2():
+    # The data variable will hold the final dataset and to be saved later
+    data = {}
+
+    # For each student
+    for i in range(question_number):
+        # We generate a student id (for example: student_12)
+        question_id ='question_' + str(i)
+        # This student will hold a structure. This will be all
+        # his answers
+        data[question_id] = {}
+        # For each question
+        result ={}# ';'.join(str(random.randint(0, 1)) for k in range(4))
+        for k in range(4):
+            result[k] = str(random.randint(0, 1))
+        data[question_id]=result
+    # Once the data is generated, we save it in a file 'dataset.json' with
+    # indentation so that the result is readable for a human
+    with open('dataset2.json', 'w') as out:
         json.dump(data, out, indent=2)
 
 if __name__ == '__main__':
-    main()
+    main2()
