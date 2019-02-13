97a5554d2876b3401e30edd3e7b9bb65b1467ab8
roman <roman.blond@edu.esiee.fr>
2019-02-13 13:27:30 +0100

    Roman I add parent = Non and now it shows some new widows but not my gridlayout

--- Project/View/home.py
+++ Project/View/home.py
@@ -1,26 +1,26 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QVBoxLayout, QGroupBox, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QVBoxLayout, QGroupBox, QPushButton, QGridLayout
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 
 class ViewHome(QWidget):
-    # def __init__(self, parent=None):
-    def __init__(self):
-        super(ViewHome(), self).__init__()
+    def __init__(self, parent=None):
+        # def __init__(self):
+        super(ViewHome, self).__init__()
         self.initUI()
 
     def initUI(self):
         # title = self.makeTitle()
         newmcq = self.makeNewMCQ()
         # prevmcq = self.makePreviousMCQ()
-        layout = QVBoxLayout()
+        layout = QGridLayout()
         # layout.addWidget(title)
         layout.addwidget(newmcq)
         # layout.addWidget(prevmcq)
         self.setLayout(layout)
         self.show()
 
-    def newmcq(self):
+    def NewMCQ(self):
         # Generate a report for a new mcq
         wid1 = QGroupBox(title='Import a new MCQ')
         layout1 = QVBoxLayout()
@@ -29,13 +29,13 @@ class ViewHome(QWidget):
         wid1.setLayout(layout1)
 
         # Content
-#        layout = QGridlayout()
-#        layout.addwidget(wid1, 0, 0)
-#        for i in range(2): layout.setRowStretch(i,1)
-#        for i in range(2): layout.setColumnStretch(i,1)
-#        content = QQwidget()
-#        content.setLayout(layout)
-#        return content
+        layout = QGridlayout()
+        layout.addwidget(wid1, 0, 0)
+        for i in range(2): layout.setRowStretch(i,1)
+        for i in range(2): layout.setColumnStretch(i,1)
+        content = QQwidget()
+        content.setLayout(layout)
+        return content
 
 
     def makeTitle(self):
@@ -51,8 +51,8 @@ class ViewHome(QWidget):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = QMainWindow()
-    view = ViewHome(window)
-    window.setCentralWidget(view)
-    window.setGeometry(700, 300, 1800, 1400)
+    # view = ViewHome(window)
+    # window.setCentralWidget(view)
+    # window.setGeometry(700, 300, 1800, 1400)
     window.show()
     sys.exit(app.exec_())
