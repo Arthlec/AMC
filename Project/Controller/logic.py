db88326fcb790d2fd249bf15ce2976ab639a12a2
evyy1 <evrard.callot@edu.esiee.fr>
2019-02-13 14:50:21 +0100

    Started the logic implementation

--- Project/Controller/logic.py
+++ Project/Controller/logic.py
@@ -1,6 +1,13 @@
+# Code about the logic implementation in the projectself.
+# This section defines the keyword used in that new language as well
+# as the syntax for the interpreter.
+# Writer: Evrard CALLOT
+
+# Example:
 # Q1 = 1 AND Q3 = 0 IS -1
 
 from enum import Enum
+import nltk
 
 class KeyWords(Enum):
     AND = 'AND'
@@ -8,25 +15,42 @@ class KeyWords(Enum):
     EQUAL = '=='
     IS = 'IS'
 
+class QuestionResult:
+    def __init__(self, id, res):
+        self.id = id
+        self.result = result
 
-class Tokenizer:
-    def __init__(self):
-        pass
 
-    def isKeyword(self, word):
-        return word in Keywords
+class LogicElement:
+    def __init__(self, questions, ops, malus):
+        self.questions = questions
+        self.ops = ops
+        self.malus = malus
 
 
 class Logic:
     def __init__(self, command):
         self.command = command
         self.interpretCommand(self.command)
-        self.tokenizer = Tokenizer()
 
-    def interpretCommand(command):
+    def interpretCommand(self, command):
+        tokens = nltk.word_tokenize(command)
+        self.checkSyntaxErrors(tokens)
+        n = len(tokens)
+        for i in range(n):
+            if tokens[i] == KeyWords.EQUAL:
+                
+
+
+    def checkSyntaxErrors(self, tokens):
+        pass
+
+    def writeLogic(self):
+        return ''
 
 
 
 if __name__ == '__main__':
     command = input('Enter a logic command:\n')
     logic = Logic(command)
+    print(logic.writeLogic())
