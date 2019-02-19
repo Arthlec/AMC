123599b8c05a868f796376d26caa9fde73a5b97f
Artlec <38135800+arthlec@users.noreply.github.com>
2019-02-19 21:21:14 +0100

    new parameters for the 4th version of the dataset

--- Project/Model/random_json_dataset.py
+++ Project/Model/random_json_dataset.py
@@ -47,15 +47,28 @@ def main2():
         data[question_id] = {}
         # For each question
         result ={}# ';'.join(str(random.randint(0, 1)) for k in range(4))
-        for k in range(4):
+        param = {} # parameters
+
+        for k in range(random.randint(1, 6)):
             result[k] = str(random.randint(0, 1))
 
+        param["e"] = 0 # non-coherent answer (multiple answer whereas simple solution)
+        param["v"] = 0 # no box is checked
+        param["d"] = 0 # offset for all non-e non-v cases
+        param["p"] = 0 # default score (the floor) given for any score below or equal to p
+        param["b"] = random.randint(1,3) # points given for a good answer
+        param["m"] = -random.randint(1,3) # negative points given for a bad answer
+        # param["auto"] = 2 # points used in an indicative question (special case we don't use)
+        param["mz"] = 0 # points given for a good answer in a strict question (all good answers or nothing)
+        param["haut"] = 0 # points given for a good answer and -1 per bad answer
+        # param["MAX"] = 0 # max of points (not needed if it is the same value as the sum of good answers)
+        # param["formula"] = 0 # useful if a formula is needed (not our case and it makes it simpler to use the other variables)
+
         data[question_id]["items"]=result
-        data[question_id]["b"] = 2
-        data[question_id]["m"] = -1
+        data[question_id]["param"]=param
     # Once the data is generated, we save it in a file 'dataset.json' with
     # indentation so that the result is readable for a human
-    with open('dataset3.json', 'w') as out:
+    with open('dataset4.json', 'w') as out:
         json.dump(data, out, indent=2)
 
 if __name__ == '__main__':
