21772a9c2b19a3f34ecd05ac660751a9fef6debb
Artlec <38135800+arthlec@users.noreply.github.com>
2019-03-04 01:15:51 +0100

    5th version of the dataset // New Data Structure closer to the AMC one

--- Project/Model/random_json_dataset.py
+++ Project/Model/random_json_dataset.py
@@ -39,36 +39,49 @@ def main2():
     data = {}
 
     # For each student
-    for i in range(question_number):
-        # We generate a student id (for example: student_12)
-        question_id ='question_' + str(i)
-        # This student will hold a structure. This will be all
-        # his answers
-        data[question_id] = {}
-        # For each question
-        result ={}# ';'.join(str(random.randint(0, 1)) for k in range(4))
-        param = {} # parameters
+    for p in range(1):      #student_number in the final version
+        data["main_strategy"] = ""
+        data["questions"] = {}
+        for i in range(question_number):
+            question_id ='question_' + str(i)
+            data["questions"][i] = {}
+            data["questions"][i]["question"] = i
+            data["questions"][i]["questionID"] = question_id
+            data["questions"][i]["type"] = 1
+            data["questions"][i]["indicative"] = 0
+            data["questions"][i]["strategy"] = ""
 
-        for k in range(random.randint(1, 6)):
-            result[k] = str(random.randint(0, 1))
+            numberOfAnswers = random.randint(2,6) # number of answers in the question
+            data["questions"][i]["answers"] = []
 
-        param["e"] = 0 # non-coherent answer (multiple answer whereas simple solution)
-        param["v"] = 0 # no box is checked
-        param["d"] = 0 # offset for all non-e non-v cases
-        param["p"] = 0 # default score (the floor) given for any score below or equal to p
-        param["b"] = random.randint(1,3) # points given for a good answer
-        param["m"] = -random.randint(1,3) # negative points given for a bad answer
-        # param["auto"] = 2 # points used in an indicative question (special case we don't use)
-        param["mz"] = 0 # points given for a good answer in a strict question (all good answers or nothing)
-        param["haut"] = 0 # points given for a good answer and -1 per bad answer
-        # param["MAX"] = 0 # max of points (not needed if it is the same value as the sum of good answers)
-        # param["formula"] = 0 # useful if a formula is needed (not our case and it makes it simpler to use the other variables)
+            for k in range(numberOfAnswers):
+                answer = {}
+                answer["question"] = i
+                answer["answer"] = k
+                answer["correct"] = random.randint(0,1)
+                answer["ticked"] = random.randint(0,1)
 
-        data[question_id]["items"]=result
-        data[question_id]["param"]=param
+                strategyForChoices = {}
+                strategyForChoices["e"] = 0 # non-coherent answer (multiple answer whereas simple solution)
+                strategyForChoices["v"] = 0 # no box is checked
+                strategyForChoices["d"] = 0 # offset for all non-e non-v cases
+                strategyForChoices["p"] = 0 # default score (the floor) given for any score below or equal to p
+                strategyForChoices["b"] = random.randint(1,3) # points given for a good answer
+                strategyForChoices["m"] = -random.randint(1,3) # negative points given for a bad answer
+                # strategyForChoices["auto"] = 2 # points used in an indicative question (special case we don't use)
+                strategyForChoices["mz"] = 0 # points given for a good answer in a strict question (all good answers or nothing)
+                strategyForChoices["haut"] = 0 # points given for a good answer and -1 per bad answer
+                # strategyForChoices["MAX"] = 0 # max of points (not needed if it is the same value as the sum of good answers)
+                # strategyForChoices["formula"] = 0 # useful if a formula is needed (not our case and it makes it simpler to use the other variables)
+
+                answer["strategy"] = strategyForChoices
+                data["questions"][i]["answers"].append(answer)
+
+
+
     # Once the data is generated, we save it in a file 'dataset.json' with
     # indentation so that the result is readable for a human
-    with open('dataset4.json', 'w') as out:
+    with open('dataset5.json', 'w') as out:
         json.dump(data, out, indent=2)
 
 if __name__ == '__main__':
