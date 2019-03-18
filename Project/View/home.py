920c3c00fce464d99fd976164dd61d26a5cf7930
saharH1989 <47110847+saharH1989@users.noreply.github.com>
2019-03-18 18:15:17 +0330

    Add view classes

--- Project/View/home.py
+++ Project/View/home.py
@@ -1,9 +1,10 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import  QScrollArea, QApplication, QTableWidget,QTableWidgetItem, QWidget, QMainWindow, QLabel, QVBoxLayout, QGroupBox, QPushButton, \
+from PyQt5.QtWidgets import  QScrollArea, QApplication, QDialog,QAction, QTableWidget,QTableWidgetItem, QWidget, QMainWindow, QLabel, QVBoxLayout, QGroupBox, QPushButton, \
     QGridLayout, QHBoxLayout, QTextEdit, QComboBox, QSlider
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont,QPolygonF, QPainter, QIcon
+#from PyQt5.QtChart import QChart, QChartView, QLineSeries
 
 from Controller.readAMC import *
 
@@ -65,6 +66,95 @@ if __name__ == '__main__':
 '''
 
 
+class AppMain(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'AMC Module'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 400
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('File')
+        editMenu = mainMenu.addMenu('Edit')
+        viewMenu = mainMenu.addMenu('View')
+        searchMenu = mainMenu.addMenu('Search')
+        toolsMenu = mainMenu.addMenu('Tools')
+        helpMenu = mainMenu.addMenu('Help')
+
+        exitButton = QAction(QIcon('exit24.png'), 'Exit', self)
+        exitButton.setShortcut('Ctrl+Q')
+        exitButton.setStatusTip('Exit application')
+        exitButton.triggered.connect(self.close)
+        fileMenu.addAction(exitButton)
+
+        self.show()
+
+class App(QDialog):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'AMC Result'
+        self.left = 10
+        self.top = 10
+        self.width = 320
+        self.height = 100
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.createGridLayout()
+
+        windowLayout = QVBoxLayout()
+        windowLayout.addWidget(self.horizontalGroupBox)
+        self.setLayout(windowLayout)
+
+        self.show()
+
+    def createGridLayout(self):
+        self.horizontalGroupBox = QGroupBox("Grid")
+        layout = QGridLayout()
+        layout.setColumnStretch(1, 4)
+        layout.setColumnStretch(2, 4)
+
+        #---------------------table view --------------------
+        scroll = QScrollArea()
+        table = QTableWidget()
+        text = QTextEdit()
+        button = QPushButton()
+        button = QPushButton('run coherence')
+        button.clicked.connect(on_click)
+        layout.addWidget(text, 0, 0)
+        layout.addWidget(button, 0, 1)
+        scroll.setWidget(table)
+        layout.addWidget(table, 1, 0)
+        layout.addWidget(window(numberOfQuestions=getNumberOfQuestions()), 1, 1)
+        text.move(20, 20)
+        text.setFixedSize(280, 30)
+        button.move(20, 80)
+        table.move(20, 100)
+
+        std, points, stdname = showPoint()
+        df = points
+        table.setColumnCount(len(df.columns))
+        table.setRowCount(len(df.index))
+        for i in range(len(df.index)):
+            for j in range(len(df.columns)):
+                table.setItem(i, j, QTableWidgetItem(str(df.iloc[i, j])))
+
+
+        self.horizontalGroupBox.setLayout(layout)
+
+
 class Example(QWidget):
 
     def __init__(self):
@@ -97,8 +187,12 @@ class window(QWidget):
     def __init__(self, parent=None, initialValue=1.0, numberOfQuestions = 1):
         super(window, self).__init__(parent)
 
+        self.horizontalGroupBox = QGroupBox("Grid")
+        self.outerL = QGridLayout()
+        self.outerL.setColumnStretch(1, 4)
+        self.outerL.setColumnStretch(2, 4)
+        #---------------------weight
         self.layout = QVBoxLayout()
-        self.listOfQuestions = []
         for i in range(1, numberOfQuestions + 1):
             self.addSlider(QLabel("Question " + str(i)), QLabel(str(initialValue)), initialValue)
 
@@ -108,8 +202,43 @@ class window(QWidget):
         self.b1.clicked.connect(self.writeWeights)
         self.layout.addWidget(self.b1)
 
+        #self.outerL.addWidget(self.layout,1,0)
+
+        # -----------------dipslay data result in table widget
+        self.scroll = QScrollArea()
+        self.table = QTableWidget()
+        self.text = QTextEdit()
+        self.button = QPushButton()
+        self.button = QPushButton('run coherence')
+        self.button.clicked.connect(on_click)
+
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+        self.scroll.setWidget(self.table)
+        self.layout.addWidget(self.table)
+        self.text.move(20, 20)
+        self.text.resize(280, 40)
+        self.button.move(20, 80)
+        self.table.move(20, 100)
+
+        std, points, stdname = showPoint()
+        #print(stdname)
+        df = points
+        #print(std.iloc[0, :])
+        #print(std.iloc[:, 0])
+
+        self.table.setColumnCount(len(df.columns))
+        self.table.setRowCount(len(df.index))
+        for i in range(len(df.index)):
+            for j in range(len(df.columns)):
+                self.table.setItem(i, j, QTableWidgetItem(str(df.iloc[i, j])))
+
+         # self.horizontalGroupBox.setLayout(outerL)
         self.setLayout(self.layout)
         self.setWindowTitle("Module AMC")
+        '''windowLayout = QVBoxLayout()
+        windowLayout.addWidget(self.horizontalGroupBox)
+        self.setLayout(windowLayout)'''
 
     def addSlider(self, title, weightText, initialValue):
         title.setAlignment(Qt.AlignCenter)
@@ -128,14 +257,18 @@ class window(QWidget):
         self.layout.addWidget(slider)
         slider.valueChanged.connect(lambda: self.valuechange(weightText, slider))
 
-        self.listOfQuestions.append(slider)
-
     def valuechange(self, weightText, slider):
         weightText.setText(str(slider.value()))
 
     def writeWeights(self):
-        for i, slider in enumerate(self.listOfQuestions):
-            changeWeight(i+1, slider.value())
+        n = 1
+        print("writeWeights 1")
+        print(self.layout.count())
+        for i in range(2, self.layout.count(), 3):
+            # print(n)
+            # print(self.layout.itemAt(i).widget().value())
+            changeWeight(n , self.layout.itemAt(i).widget().value())
+            n += 1
         print(getWeights())
 
 class DoubleSlider(QSlider):
@@ -188,39 +321,8 @@ def on_click():
 if __name__ == '__main__':
     
     computeData()
-    
     app = QApplication(sys.argv)
-    #-----------------dipslay data result in table widget
-    win = QWidget()
-    scroll = QScrollArea()
-    layout = QVBoxLayout()
-    table = QTableWidget()
-    text = QTextEdit()
-    button=QPushButton()
-    button = QPushButton('run coherence')
-
-    button.clicked.connect(on_click)
-    button.clicked.connect(on_click)
-
-    layout.addWidget(text)
-    layout.addWidget(button)
-    scroll.setWidget(table)
-    layout.addWidget(table)
-    text.move(20, 20)
-    text.resize(280, 40)
-    button.move(20, 80)
-    table.move(20,100)
-    win.setLayout(layout)
-    std,points=showPoint()
-    df = points
-    table.setColumnCount(len(df.columns))
-    table.setRowCount(len(df.index))
-    for i in range(len(df.index)):
-        for j in range(len(df.columns)):
-            table.setItem(i, j, QTableWidgetItem(str(df.iloc[i, j])))
-
-    win.show()
-    #-----------------end display in table view
-    ex = window(numberOfQuestions=getNumberOfQuestions())
-    ex.show()
+   # ex = window(numberOfQuestions=getNumberOfQuestions())
+   # ex.show()
+    ex1 = AppMain()
     sys.exit(app.exec_())
