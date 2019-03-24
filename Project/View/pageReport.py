785aea604b8922c7853bba9141cbcb3cf37860d7
saharH1989 <47110847+saharH1989@users.noreply.github.com>
2019-03-24 11:26:38 +0100

    Sahar: Report page with table view text box and chart
    everybody could add related function to this page,  
    apply coherence and update result 
    display related chart
    apply weight and update result

--- Project/View/pageReport.py
+++ Project/View/pageReport.py
@@ -1,12 +1,38 @@
+#+----------------------------------------------------+
+#| 23/03/2019 - Report page for teachers
+#| Created by Sahar Hosseini - Roman Blond
+#| description,
+#| report data as a table, chart and teachers enable to apply coherence, weight and penalty
+#+----------------------------------------------------+
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QScrollArea, QApplication, QDialog, QAction, QTableWidget, QTableWidgetItem, QWidget, \
+from PyQt5.QtWidgets import QScrollArea, QApplication, QDialog, QLineEdit, QAction, QTableWidget, QTableWidgetItem, QWidget, \
     QMainWindow, QLabel, QVBoxLayout, QGroupBox, QPushButton, \
-    QGridLayout, QHBoxLayout, QTextEdit, QComboBox, QSlider
+    QGridLayout, QHBoxLayout, QTextEdit, QComboBox, QSizePolicy, QSlider
 from PyQt5.QtCore import Qt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 from Project.Controller.readAMC import *
 import numpy as np
 
+#+--------------global data that uses in this page
+points, stdname = computeData()
+df = stdname.as_matrix()
+score_chart = df[10,:].astype(int)
+print(score_chart)
+mark_chart = np.unique(score_chart)
+print(mark_chart)
+eff_chart = []
+
+for i in range(len(mark_chart)):
+    effective_chart = []
+    effective_chart = np.count_nonzero(score_chart == mark_chart[i])
+    eff_chart = np.append(eff_chart, effective_chart)
+print(eff_chart.astype(int))
+X = mark_chart
+Y = eff_chart
+X_pie = ['8','9','12','13','14','15','16']
+
+#+--------------main class
 class App(QDialog):
 
     def __init__(self):
@@ -21,90 +47,81 @@ class App(QDialog):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-
         self.createGridLayout()
-
         windowLayout = QVBoxLayout()
         windowLayout.addWidget(self.horizontalGroupBox)
         self.setLayout(windowLayout)
-
         self.show()
 
     def createGridLayout(self):
-        self.horizontalGroupBox = QGroupBox("Grid")
+        self.horizontalGroupBox = QGroupBox("AMC report for teachers")
+
+        # ---------------------grid layout --------------------
         layout = QGridLayout()
-        layout.setColumnStretch(1, 4)
-        layout.setColumnStretch(2, 4)
+        layout.setColumnStretch(1, 3)
+        layout.setColumnStretch(2, 3)
 
+        # ---------------------text boxes  --------------------
+        txtCoherence = QLineEdit("Please Enter your Coherence Formula")
+        txtCoherence.resize(20, 20)
+        layout.addWidget(txtCoherence, 0, 0)
+        cbChart = QComboBox()
+        cbChart.addItem("Plot Chart")
+        cbChart.addItem("Bar Chart")
+        cbChart.addItem("Line Chart")
+        cbChart.addItem("Pie Chart")
+        cbChart.resize(140, 30)
+        layout.addWidget(cbChart, 0, 2)
+        btnApply = QPushButton("Apply Coherence")
+        btnApply.clicked.connect(self.onClickApply)
+        layout.addWidget(btnApply, 0, 1)
+        cbChart.currentIndexChanged.connect(self.OnChangeCbChart)
         # ---------------------table view --------------------
-        '''scroll = QScrollArea()
+        scroll = QScrollArea()
         table = QTableWidget()
-        text = QTextEdit()
-        button = QPushButton()
-        button = QPushButton('run coherence')
-        button.clicked.connect(on_click)
-        layout.addWidget(text, 0, 0)
-        layout.addWidget(button, 0, 1)
         scroll.setWidget(table)
-        layout.addWidget(table, 1, 0)'''
-        numberOfQuestions, arrCorrectAns = getNumberOfQuestions()
-        layout.addWidget(window(arrCorrectAns=arrCorrectAns,numberOfQuestions=numberOfQuestions), 1, 1)
-        ''' text.move(20, 20)
-        text.setFixedSize(280, 30)
-        button.move(20, 80)
-        table.move(20, 100)
+        layout.addWidget(table, 1, 0)
 
-        points, stdname = showPoint()
-        df = points
+        df = stdname.T # main data
+        colName = []  # column name
+        rowName = []  # row name
         table.setColumnCount(len(df.columns))
         table.setRowCount(len(df.index))
         for i in range(len(df.index)):
+            colName.append(str(df.index[i]))
             for j in range(len(df.columns)):
-                table.setItem(i, j, QTableWidgetItem(str(df.iloc[i, j])))'''
+                rowName.append(str(df.columns[j]))
+                table.setItem(i, j, QTableWidgetItem(str(round(df.iloc[i, j], 1))))
+
+        table.setHorizontalHeaderLabels(rowName)
+        table.setVerticalHeaderLabels(colName)
+        table.resizeRowsToContents()
+        table.resizeColumnsToContents()
+
+        # ---------------------slider  weight --------------------
+        numberOfQuestions, arrCorrectAns = getNumberOfQuestions()
+        layout.addWidget(buildSlider(arrCorrectAns=arrCorrectAns,numberOfQuestions=numberOfQuestions), 1, 1)
+
+        # ---------------------chart view --------------------
+        layout.addWidget(PlotCanvas(), 1, 2)
 
         self.horizontalGroupBox.setLayout(layout)
 
+    def onClickApply(self):
+        print("click: run your coherence and update data ")
 
-class Example(QWidget):
+    def OnChangeCbChart(self):
+        print("click: display your chart related to seelcted option ")
 
-    def __init__(self):
-        super().__init__()
-
-        self.initUI()
-
-    def initUI(self):
-        makenewmcq = QPushButton("Make a new MCQ")
-        prevmcq = QGroupBox("show previous MCQ")
-
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(makenewmcq)
-
-        vbox1 = QVBoxLayout()
-        vbox1.addWidget(prevmcq)
-        vbox1.addStretch(800)
-        vbox1.addLayout(hbox)
-
-        self.setLayout(vbox1)
-
-        self.setGeometry(300, 300, 300, 150)
-        self.setWindowTitle('Make a new MCQ')
-        self.resize(400, 400)
-        self.show()
-
-
-class window(QWidget):
+#+--------------builder slider has written by Arthur Lecert
+class buildSlider(QWidget):
     def __init__(self, parent=None, initialValue=1.0, arrCorrectAns=[], numberOfQuestions=1):
-        super(window, self).__init__(parent)
+        super(buildSlider, self).__init__(parent)
 
-        self.horizontalGroupBox = QGroupBox("Grid")
-        self.outerL = QGridLayout()
-        self.outerL.setColumnStretch(1, 4)
-        self.outerL.setColumnStretch(2, 4)
         # ---------------------weight
         self.layout = QVBoxLayout()
         for i in range(numberOfQuestions):#(1, numberOfQuestions + 1):
-            self.addSlider(QLabel("Question " + str(i+1) +"  correctness" + str(arrCorrectAns[i]) + "  %"), QLabel(str(initialValue)), initialValue)
+            self.addSlider(QLabel("Question " + str(i+1) +"  correctness: " + str(arrCorrectAns[i]) + "  %"), QLabel(str(initialValue)), initialValue)
 
         self.b1 = QPushButton("Save weight")
         self.b1.setCheckable(True)
@@ -195,7 +212,56 @@ def on_click(self):
        textboxValue = self.textbox.text()
        print(textboxValue)
        print("run coherence")
+#+--------------chart class has written by Roman Blond
+class Chart(QMainWindow):
 
+    def __init__(self):
+        super().__init__()
+        self.left = 100
+        self.top = 100
+        self.title = 'Repartition of score for this MCQ'
+        self.width = 600
+        self.height = 400
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        m = PlotCanvas(self, width=5, height=4)
+        m.move(0,0)
+
+        self.show()
+
+
+class PlotCanvas(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+        FigureCanvas.setSizePolicy(self,
+                QSizePolicy.Expanding,
+                QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+        # self.plot_histogram()
+        # self.plot_pie()
+        self.plot_box()
+    def plot_histogram(self):
+        ax = self.figure.add_subplot(111)
+        ax.plot(X, Y)
+        ax.set_title('Repartition of score in the class')
+        self.draw()
+    def plot_pie(self):
+        ax = self.figure.add_subplot(111)
+        ax.pie(Y, labels=X_pie)
+        ax.set_title('Repartition of score in the class')
+        self.draw()
+    def plot_box(self):
+        ax = self.figure.add_subplot(111)
+        ax.boxplot(X)
+        ax.set_title('Repartition of score in the class')
+        self.draw()
 
 if __name__ == '__main__':
 
