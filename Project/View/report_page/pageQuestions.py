ab03dde16c4b5f31b1f2bc1f634fc04014f31712
saharH1989 <47110847+saharH1989@users.noreply.github.com>
2019-04-08 03:45:23 +0200

    Sahar: complete navigation form to show single view
    question,
    apply add weight and penalty 
    I do not know how to design it ?!

--- Project/View/report_page/pageQuestions.py
+++ Project/View/report_page/pageQuestions.py
@@ -8,123 +8,123 @@ import sys
 
 from PyQt5.QtCore import pyqtSignal, Qt
 from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit, QPushButton,
-                             QCheckBox, QMessageBox, QComboBox,QSlider,QGroupBox, QVBoxLayout, QGridLayout, QApplication)
+                             QCheckBox, QMessageBox,QFormLayout,QDialogButtonBox,QSpinBox,QHBoxLayout, QComboBox,QSlider,QGroupBox, QVBoxLayout, QGridLayout, QApplication)
 
 from Project.Controller.logic.logic import *
 from Project.Controller.readAMC import *
 
-#+--------------main class
+
 class lstQuestion(QWidget):
-    b, c, v = computeData2()
-    print(b)  # include questions and number of choices
-    print(c)
-    print(v)  # use this one
+   b, c, v = computeData2()
+   print(v)
+   def __init__(self):
+       super().__init__()
+       self.currentIndex = 0
+       self.lenData= len(self.v) -1
+       self.initUI()
 
-    def __init__(self):
-        super().__init__()
-        self.title = 'AMC Question Report'
-        self.left = 30
-        self.top = 30
-        self.width = 600
-        self.height = 400
-        self.currentIndex = 0
-        self.initUI()
-
-    def initUI(self):
-        print("---------initUI---------")
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-
-        btnFirst = QPushButton("First")
-        btnPre = QPushButton("Previous")
-        btnNext = QPushButton("Next")
-        btnLast = QPushButton("Last")
-        btnFirst.clicked.connect(self.goFirst)
-        btnPre.clicked.connect(self.goPre)
-        btnNext.clicked.connect(self.goNext)
-        btnLast.clicked.connect(self.goLast)
-
-
-        self.createQuestionView(self.currentIndex)
-        self.windowLayout = QGridLayout()
-        self.windowLayout.addWidget(self.horizontalGroupBox,0,0)
-        self.windowLayout.addWidget(btnFirst,  1, 0)
-        self.windowLayout.addWidget(btnPre, 1, 1)
-        self.windowLayout.addWidget(btnNext, 1, 2)
-        self.windowLayout.addWidget(btnLast,  1, 3)
-        self.setLayout(self.windowLayout)
+   def initUI(self):
+        self.createFormGroupBox(self.currentIndex)
+        self.grid = QGridLayout()
+        self.grid.addWidget(self.formGroupBox,0,0)
+        self.grid.addWidget(self.createBtnGroup(), 1, 0)
+        self.setLayout(self.grid)
+        self.setWindowTitle("Quetsion Report")
         self.show()
-    def createQuestionView(self,Index):
-        print("Index")
-        print(Index)
-        self.horizontalGroupBox = QGroupBox("AMC report for teachers")
 
-        # ---------------------grid layout --------------------
-        self.layout = QGridLayout()
-        self.layout.setColumnStretch(0, 3)
-        self.layout.setColumnStretch(1, 3)
-        self.layout.setColumnStretch(2, 3)
-        #main part
-        # ---------------questions label
+   def createBtnGroup(self):
+       groupBox = QGroupBox()
+       btnFirst = QPushButton("First")
+       btnPre = QPushButton("Previous")
+       btnNext = QPushButton("Next")
+       btnLast = QPushButton("Last")
+       btnFirst.clicked.connect(self.goFirst)
+       btnPre.clicked.connect(self.goPre)
+       btnNext.clicked.connect(self.goNext)
+       btnLast.clicked.connect(self.goLast)
+       hbox = QHBoxLayout()
+       hbox.addWidget(btnFirst)
+       hbox.addWidget(btnPre)
+       hbox.addWidget(btnNext)
+       hbox.addWidget(btnLast)
+       hbox.addStretch(1)
+       groupBox.setLayout(hbox)
+       return groupBox
+
+   def createFormGroupBox(self, Index):
+        print("new index")
+        print(Index)
+        self.formGroupBox = QGroupBox()
+        layout = QFormLayout()
         row = self.v.iloc[Index]
         qNum = row[1]
         qChoices = row[2]
-        print(row)
-        print(qNum)
-        self.lblQuestion = QLabel('Question ' + str(qNum) + ': How new marking module works?  ')
-        self.lblQuestion1 = QLabel('!!!!show correctness of perecentage of this question %%% or chart ')
+        lblQuestion = QLabel('Question ' + str(qNum) + ': text of question ' + str(qNum) + '?  ')
 
-        self.slider = QSlider(Qt.Horizontal)
-        self.slider.setFocusPolicy(Qt.StrongFocus)
-        self.slider.setTickPosition(QSlider.TicksBothSides)
-        self.slider.setTickInterval(20)
+        txtCoher = QLineEdit("Coherence Formula")
+        txtCoher.resize(20, 20)
 
-        self.slider.setSingleStep(1)
+        slider = QSlider(Qt.Horizontal)
+        slider.setTickPosition(QSlider.TicksBelow)
+        slider.setFocusPolicy(Qt.StrongFocus)
+        slider.setTickPosition(QSlider.TicksBothSides)
+        slider.setTickInterval(0.1)
+        slider.setMinimum(0.0)
+        slider.setMaximum(2.0)
+        slider.setValue(1.0)
+        slider.valueChanged.connect(self.addWeight)
 
-        self.txtCoher = QLineEdit("Coherence Formula")
-        self.txtCoher.resize(20, 20)
-        # ---------------question choices
-        self.chkList = []
-
+        penalty = QSpinBox()
+        chkList = []
         for i in range(qChoices):
-            self.chkList.append(QCheckBox("Option " + str(i + 1)))
+            chkList.append(QCheckBox("Option " + str(i + 1)))
 
-        self.layout.addWidget(self.lblQuestion, 0, 0)
-        #layout.addWidget(self.lblQuestion1, 1, 2)
-        self.layout.addWidget(self.slider, 1, 1)
-        self.layout.addWidget(self.txtCoher, 0, 1)
+        layout.addRow(lblQuestion, txtCoher)
+        layout.addRow(QLabel("Change the weight:"), slider)
+        #layout.addRow(QLabel("Change Penalty:"), penalty)
         for i in range(qChoices):
-            self.layout.addWidget(self.chkList[i], i + 1, 0)
+            layout.addRow(chkList[i])
 
+        self.formGroupBox.setLayout(layout)
 
-        self.horizontalGroupBox.setLayout(self.layout)
-    def goFirst(self):
-         if self.currentIndex==0:
-             print("your are in first ")
-         else:
-             self.currentIndex=0
+   #-----------------------------function defination-----------------------
+   def  addWeight(self):
+       print("add weight")
 
-         self.initUI()
+   def clearForm(self,Index):
+       self.formGroupBox.deleteLater()
+       self.createFormGroupBox(Index)
+       self.grid.addWidget(self.formGroupBox, 0, 0)
+   #-------------------------navigation ------------------------------------
+   def goFirst(self):
+       if self.currentIndex == 0:
+           print("your are in first ")
+       else:
+           self.currentIndex = 0
+           self.clearForm(self.currentIndex)
 
-    def goPre(self):
-         print("")
+   def goPre(self):
+       if self.currentIndex == 0:
+           print("your are in first ")
+       else:
+           self.currentIndex = self.currentIndex - 1
+           self.clearForm(self.currentIndex)
 
-    def goNext(self):
-        print("next1")
-        if self.currentIndex == len(self.v):
-            print("next2")
-            print("your are in last ")
-        else:
-            print("next3")
-            self.currentIndex = self.currentIndex +1
-            print(" self current index value next4")
-        print(self.currentIndex)
-        print("next5")
-        self.createQuestionView(self.currentIndex)
-        #self.initUI()
+   def goNext(self):
+       if self.currentIndex == self.lenData:
+           print("your are in last ")
+       else:
+           self.currentIndex = self.currentIndex + 1
+           self.clearForm(self.currentIndex)
 
-    def goLast(self):
-        print("")
+   def goLast(self):
+       print(len(self.v))
+       if self.currentIndex == self.lenData:
+           print("your are in last ")
+       else:
+           self.currentIndex = self.lenData
+           self.clearForm(self.currentIndex)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
