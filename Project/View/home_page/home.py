250c0a47c9f8dd82998fc4b80663422b1ff0959d
evyy1 <evrard.callot@edu.esiee.fr>
2019-04-09 09:38:29 +0200

    First example of home page

--- Project/View/home_page/home.py
+++ Project/View/home_page/home.py
@@ -1,30 +1,111 @@
 
 import sys
-from PyQt5.QtWidgets import QWidget, QAction
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import Qt
 
 from Controller.readAMC import *
+
+
+class _ListItem(QWidget):
+    def __init__(self, name, path):
+        super(_ListItem, self).__init__()
+        self.name = name
+        self.path = path
+
+        titleFont = QFont()
+        titleFont.setBold(True)
+        titleFont.setPointSize(15)
+
+        pathFont = QFont()
+        pathFont.setPointSize(10)
+        pathFont.setItalic(True)
+
+        verticalLayout = QVBoxLayout()
+        self.setLayout(verticalLayout)
+
+        titleLabel = QLabel(self.name)
+        titleLabel.setFont(titleFont)
+        pathLabel = QLabel(self.path)
+        pathLabel.setFont(pathFont)
+        verticalLayout.addWidget(titleLabel)
+        verticalLayout.addWidget(pathLabel)
 
 
 class HomePage(QWidget):
     def initUI(self, mainWindow):
         mainWindow.setWindowTitle('AMC Module')
 
-        mainMenu = mainWindow.menuBar()
-        fileMenu = mainMenu.addMenu('File')
-        editMenu = mainMenu.addMenu('Edit')
-        viewMenu = mainMenu.addMenu('View')
-        searchMenu = mainMenu.addMenu('Search')
-        toolsMenu = mainMenu.addMenu('Tools')
-        helpMenu = mainMenu.addMenu('Help')
+        verticalLayout = QVBoxLayout()
+        verticalLayout.addWidget(self.createTitle())
+        verticalLayout.addWidget(self.createInterface())
+        self.setLayout(verticalLayout)
+        mainWindow.setCentralWidget(self)
 
-        exitButton = QAction(QIcon('exit24.png'), 'Exit', self)
-        exitButton.setShortcut('Ctrl+Q')
-        exitButton.setStatusTip('Exit application')
-        exitButton.triggered.connect(mainWindow.close)
-        fileMenu.addAction(exitButton)
 
-        self.show()
+    def createTitle(self):
+        titleLabel = QLabel('Welcome to the MCQ correction tool')
+        titleLabel.setAlignment(Qt.AlignCenter)
+
+        titleFont = QFont()
+        titleFont.setBold(True)
+        titleFont.setPointSize(25)
+
+        titleLabel.setFont(titleFont)
+        return titleLabel
+
+    def createInterface(self):
+        widget = QWidget()
+
+        grid = QGridLayout()
+        widget.setLayout(grid)
+
+        grid.addWidget(self.createNewCorrectionButton(), 1, 0, Qt.AlignCenter)
+        grid.addWidget(self.createOldCorrectionsList(), 1, 1)
+
+        for i in range(2):
+            grid.setColumnStretch(i, 1)
+            grid.setRowStretch(i, 1)
+
+        return widget
+
+    def createNewCorrectionButton(self):
+        button = QPushButton('New correction')
+        button.setStyleSheet("""
+            QPushButton {
+                    width: 30px
+                    height: 100px
+            }
+
+        """)
+
+        return button
+
+
+    def createOldCorrectionsList(self):
+        fakeList = [
+            ['Exam8', 'C:/User/user/AMC/exam8'],
+            ['Exam7', 'C:/User/user/AMC/exam7'],
+            ['Exam6', 'C:/User/user/AMC/exam6'],
+            ['Exam5', 'C:/User/user/AMC/exam5'],
+            ['Exam4', 'C:/User/user/AMC/exam4'],
+            ['Exam3', 'C:/User/user/AMC/exam3'],
+            ['Exam2', 'C:/User/user/AMC/exam2'],
+            ['Exam1', 'C:/User/user/AMC/exam1'],
+        ]
+
+        widget = QWidget()
+        verticalLayout = QVBoxLayout()
+        widget.setLayout(verticalLayout)
+        for item in fakeList:
+            verticalLayout.addWidget(_ListItem(item[0], item[1]))
+
+        scrollArea = QScrollArea()
+        scrollArea.setWidget(widget)
+
+        return scrollArea
+
+
 
 class App(QWidget):
 
@@ -112,15 +193,7 @@ class Example(QWidget):
 
 
 if __name__ == '__main__':
-    boxes , resultatsPoints = computeData()
-    print(boxes)
-    print(resultatsPoints)
-    boxes, resultatsPoints = updateData()
-    # print(resultatsPoints)
-    # print(getWeights())
-    # app = QApplication(sys.argv)
-    # ex = window(numberOfQuestions=getNumberOfQuestions())
-    # ex.show()
-    # ex1 = AppMain()
-    # ex = Chart()
-    # sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    w = QMainWindow()
+    w.showMaximized()
+    sys.exit(app.exec_())
