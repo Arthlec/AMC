d3319979ec8dddc15b4524f52b08d43444f66d23
evyy1 <evrard.callot@edu.esiee.fr>
2019-04-09 10:25:58 +0200

    Connection between Home Page and Settings page OK

--- Project/View/home_page/home.py
+++ Project/View/home_page/home.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import Qt
 
-from Controller.readAMC import *
+from View.setting_page.pageSetting import Setting
 
 
 class _ListItem(QWidget):
@@ -32,15 +32,17 @@ class _ListItem(QWidget):
         verticalLayout.addWidget(pathLabel)
 
 
+
 class HomePage(QWidget):
     def initUI(self, mainWindow):
+        self.mainWindow = mainWindow
         mainWindow.setWindowTitle('AMC Module')
 
         verticalLayout = QVBoxLayout()
         verticalLayout.addWidget(self.createTitle())
         verticalLayout.addWidget(self.createInterface())
         self.setLayout(verticalLayout)
-        mainWindow.setCentralWidget(self)
+        self.mainWindow.setCentralWidget(self)
 
 
     def createTitle(self):
@@ -70,16 +72,19 @@ class HomePage(QWidget):
         return widget
 
     def createNewCorrectionButton(self):
-        button = QPushButton('New correction')
-        button.setStyleSheet("""
-            QPushButton {
-                    width: 30px
-                    height: 100px
-            }
+        self.newCorrectionButton = QPushButton('New correction')
+        self.newCorrectionButton.setFixedSize(300, 100)
+        font = QFont()
+        font.setPointSize(12)
+        self.newCorrectionButton.setFont(font)
+        self.newCorrectionButton.clicked.connect(self.openSettings)
 
-        """)
+        return self.newCorrectionButton
 
-        return button
+    def openSettings(self):
+        settings = Setting()
+        settings.initUI(self.mainWindow)
+        self.mainWindow.show()
 
 
     def createOldCorrectionsList(self):
@@ -105,91 +110,6 @@ class HomePage(QWidget):
 
         return scrollArea
 
-
-
-class App(QWidget):
-
-    def __init__(self, parent=None):
-        super(App, super).__init__(parent)
-        self.title = 'AMC Result'
-        self.left = 10
-        self.top = 10
-        self.width = 320
-        self.height = 100
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-
-        self.createGridLayout()
-
-        windowLayout = QVBoxLayout()
-        windowLayout.addWidget(self.horizontalGroupBox)
-        self.setLayout(windowLayout)
-
-        self.show()
-
-    def createGridLayout(self):
-        self.horizontalGroupBox = QGroupBox("Grid")
-        layout = QGridLayout()
-        layout.setColumnStretch(1, 4)
-        layout.setColumnStretch(2, 4)
-
-        #---------------------table view --------------------
-        scroll = QScrollArea()
-        table = QTableWidget()
-        text = QTextEdit()
-        button = QPushButton()
-        button = QPushButton('run coherence')
-        button.clicked.connect(on_click)
-        layout.addWidget(text, 0, 0)
-        layout.addWidget(button, 0, 1)
-        scroll.setWidget(table)
-        layout.addWidget(table, 1, 0)
-        layout.addWidget(window(numberOfQuestions=getNumberOfQuestions()), 1, 1)
-        text.move(20, 20)
-        text.setFixedSize(280, 30)
-        button.move(20, 80)
-        table.move(20, 100)
-
-        std, points, stdname = showPoint()
-        df = points
-        table.setColumnCount(len(df.columns))
-        table.setRowCount(len(df.index))
-        for i in range(len(df.index)):
-            for j in range(len(df.columns)):
-                table.setItem(i, j, QTableWidgetItem(str(df.iloc[i, j])))
-
-
-        self.horizontalGroupBox.setLayout(layout)
-
-
-class Example(QWidget):
-    def __init__(self, parent=None):
-        super(Example, self).__init__(parent)
-
-        self.initUI()
-
-    def initUI(self):
-        makenewmcq = QPushButton("Make a new MCQ")
-        prevmcq = QGroupBox("show previous MCQ")
-
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(makenewmcq)
-
-        vbox1 = QVBoxLayout()
-        vbox1.addWidget(prevmcq)
-        vbox1.addStretch(800)
-        vbox1.addLayout(hbox)
-
-        self.setLayout(vbox1)
-
-        self.setGeometry(300, 300, 300, 150)
-        self.setWindowTitle('Make a new MCQ')
-        self.resize(400, 400)
-        self.show()
 
 
 if __name__ == '__main__':
